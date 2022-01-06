1b2f9e31fcad2f18b719321131bb36be78a04476
Caique Dourado <eng.caiquedourado@gmail.com>
2022-01-06 10:08:28 -0300

    feat - construindo o primeiro widget :construction:

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(ProjetoPerguntas());
+}
+
+class ProjetoPerguntas extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter!!!'),
+    );
+  }
+}
