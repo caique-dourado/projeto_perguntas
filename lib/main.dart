7926413c2ded22086fc79d4b30522020674a1a00
Caique Dourado <eng.caiquedourado@gmail.com>
2022-01-06 11:04:33 -0300

    feat: organizando os elementos e adcionando buttons :construction:

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: const Text('Perguntas'),
       ),
-      body: const Text('Olá Flutter!!!'),
+      body: Column(
+        children: <Widget>[
+          Text(perguntas[0]),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 2')),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 3'))
+        ],
+      ),
     ));
   }
 }
