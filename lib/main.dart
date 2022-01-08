40ddbdda38f9b79fbbf33361e0961853903c60ee
caique-dourado <eng.caiquedourado@gmail.com>
2022-01-08 09:50:19 -0300

    feat: chamando a função CallBack :constructor:

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
       body: Column(
         children: <Widget>[
           Questao(perguntas[_perguntaSelecionada]),
-          const Resposta('Resposta 1'),
-          const Resposta('Resposta 2'),
-          const Resposta('Resposta 3'),
+          Resposta('Resposta 1', _responder),
+          Resposta('Resposta 2', _responder),
+          Resposta('Resposta 3', _responder),
         ],
       ),
     ));
