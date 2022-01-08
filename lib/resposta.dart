8e85906f6ab0478007fc7d86741a50c375b438c7
caique-dourado <eng.caiquedourado@gmail.com>
2022-01-08 09:48:45 -0300

    feat: passando a fução Callback

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-  const Resposta(this.texto);
+  final void Function() quandoSelecionada;
+  const Resposta(this.texto, this.quandoSelecionada, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: null, child: Text(texto));
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+            textStyle: MaterialStateProperty.all(
+                const TextStyle(color: Colors.white))),
+        onPressed: quandoSelecionada,
+        child: Text(texto),
+      ),
+    );
   }
 }
